188c682a705ea3c65c95658ca8ccc5db0780e0d9
michelle9193 <michellefortuin93@gmail.com>
2019-03-24 17:51:40 +0200

    added 2 products to shop page

--- js/main.js
+++ js/main.js
@@ -6,16 +6,18 @@ function Clothing(image, caption, price, details) {
 }
 
 
-// first product
-let maxiDress = new Clothing ('<img src="images/0131265647443_001_d.jpg">',"Black Maxi Dress", "R499", "Slip        into this ultra-luxe maxi dress. Satin-look fabric cut in a classic slip silhouette with seamed triangle cups at the empire waist. Topped skinny shoulder straps at the v-neckline that tie at the open back.");
+// 1.
+let maxiDress = new Clothing ('<img src="images/0131265647443_001_d.jpg">',"Black Maxi Dress", "R499", "Slip into this ultra-luxe maxi dress. Satin-look fabric cut in a classic slip silhouette with seamed triangle cups at the empire waist. Topped skinny shoulder straps at the v-neckline that tie at the open back.");
 
-//second product
-    dungaree = new Clothing('<img src="images/0180621400087_020_b.jpg">',"Brown Dungaree", "R699", "Lorem ");
+//2. 
+    dungaree = new Clothing('<img src="images/0180621400087_020_b.jpg">',"Cassidy Brown Bib Dungaree", "R699", "Essential workwear-look dungarees. Features a classic bib style front pocket. Allover contrast stitch detailing. Topped with adjustable shoulder straps. ");
+
+// 3. 
+    purpleHoodie = new Clothing("Purple Crop Hoodie", "R499");
 
 
 
-    // purpleHoodie = new Clothing("Purple Crop Hoodie", 499);
-    // highJeans = new Clothing("High Waisted Jeans", 599);
+// highJeans = new Clothing("High Waisted Jeans", 599);
     // bomberJacket = new Clothing("Bomber Jacket", 999);
     // colorfulCrop = new Clothing("Colorful Crop Top", 399);
     // insetDress = new Clothing("Color Block Inset Dress", 499);
@@ -27,7 +29,7 @@ let image = document.getElementById("image");
     details = document.getElementById("details");
 
 
-// First Product
+// Black Maxi Dress 
 image.innerHTML = maxiDress.image;
 caption.innerHTML = maxiDress.caption;
 price.innerHTML = maxiDress.price;
